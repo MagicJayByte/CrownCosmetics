b9dcccc1f4f533efc03de6b4741b87612fb9e8c1
JakubDudziak <jakubdudziak123@gmail.com>
2023-06-17 21:54:16 +0200

    wszystko

--- src/main/java/com/uep/wap/securingweb/WebSecurityConfig.java
+++ src/main/java/com/uep/wap/securingweb/WebSecurityConfig.java
@@ -1,25 +1,18 @@
 package com.uep.wap.securingweb;
 
-import com.uep.wap.service.CustomUserDetailsService;
-import com.uep.wap.service.UsersService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -38,14 +31,14 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                        authorize.antMatchers("/").permitAll()
+                        authorize.antMatchers("/").authenticated()
                                 .antMatchers("/index").permitAll()
                                 .antMatchers("/users").permitAll()
                 ).formLogin(
                         form -> form
                                 .loginPage("/login")
                                 .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/users")
+                                .defaultSuccessUrl("/shop")
                                 .permitAll()
                 ).logout(
                         logout -> logout
